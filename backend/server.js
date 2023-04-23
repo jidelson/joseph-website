2237d5f2ab0c1b9b1e54c5195f2dad487e5e058f
jidelson <joeidelson@gmail.com>
2023-04-22 20:01:10 -0500

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,15 @@ const app = express();
 
 app.use(express.static(path.join(__dirname + "/public")));
 
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'This is the api endpoint'
+    })
+  })
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.listen(PORT, function(){
+    console.log(`server started on port ${PORT} `)
+  })
+  
