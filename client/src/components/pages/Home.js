c54f3abcb3cba0d2e49303637665bfa07d90d1bb
jidelson <joeidelson@gmail.com>
2024-03-24 00:18:48 -0500

    updated tech skills

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -23,6 +23,8 @@ import mysql from '../../images/mysql.png';
 import github from '../../images/github.png';
 import spring from '../../images/spring.png';
 import angular from '../../images/angular.png';
+import kibana from '../../images/kibana.jpg';
+import cassandra from '../../images/cassandra.jpg';
 
 import dollar from '../../images/dollar.jpg';
 import flashcards from '../../images/flashcards.jpg';
@@ -275,6 +277,26 @@ function Home() {
                                 </div>
                                 <div className="card-body text-center">
                                     <h4 className="card-title font-roboto">Angular</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <div className="skills">
+                                <div className="skills-img text-center-py-4">
+                                    <img src={kibana} alt="" className="rounded mx-auto d-block img-fluid" />
+                                </div>
+                                <div className="card-body text-center">
+                                    <h4 className="card-title font-roboto">Kibana</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <div className="skills">
+                                <div className="skills-img text-center-py-4">
+                                    <img src={cassandra} alt="" className="rounded mx-auto d-block img-fluid" />
+                                </div>
+                                <div className="card-body text-center">
+                                    <h4 className="card-title font-roboto">Cassandra</h4>
                                 </div>
                             </div>
                         </div>
